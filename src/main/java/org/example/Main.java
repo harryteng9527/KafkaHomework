24c1e0bc1255dc03a89e445af512313f0f4cfae5
Zhi-Mao Teng <zhimao.teng@gmail.com>
2021-12-27 20:19:36 +0800

    delete SplitArgs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,24 @@
 package org.example;
 import org.apache.kafka.clients.producer.*;
 
+import java.util.ListIterator;
 import java.util.Properties;
+
+import static java.lang.String.format;
 
 public class Main {
     public static void main(String[] args) {
         int argsSize = args.length;
         int i = 0;
         String topicName = null;
-        int records, recordSize;
+        String key = null, value = null;
+        int records=0, recordSize=0;
         Properties props = new Properties();
 
         for(i = 0 ; i < argsSize ; i++){
             switch (args[i]) {
                 case "--brokers":
-                    props.put("bootstrap.servers", args[++i]);
+                    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, args[++i]);
                     break;
                 case "--topic":
                     topicName = args[++i];
@@ -28,18 +32,17 @@ public class Main {
             }
         }
 
-        props.put("acks", "1");
-        props.put("retries", 1);
-        props.put("linger.ms", 10);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Configuration conf = new Configuration();
+        conf.config(props);
 
         Producer<String, String> producer = new KafkaProducer<>(props);
-        for (i = 0; i < 10000; i++)
-            producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(i), Integer.toString(i)));
-
+        for (i = 0; i < records; i++) {
+            key = String.format("key-%06d", i);
+            value = String.format("value-%06d",i);
+            //producer.send(new ProducerRecord<String, String>(topicName, null,key, value,));
+        }
         producer.close();
 
-        System.out.println("walwala");
+        System.out.println("walawalaa");
     }
 }
